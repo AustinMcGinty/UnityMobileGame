f76857afb87dbdc10eb8fe8aa36a3f1071924f06
Austin McGinty <apmcginty@email.msmary.edu>
2022-05-01 16:08:23 -0400

    Made Changes to update unity and changed platform to Android from windows. While also chaning PlayerBehavior with mouse input.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -33,6 +33,32 @@ public class PlayerBehaviour : MonoBehaviour
     }
 
     /// <summary>
+    /// Will Figure out where to move the player horizontally
+    /// </summary>
+    /// <param name="pixelPos">The position the player has touched/clicked on</param>
+    /// <returns>The direction to move in the x axis</returns>
+    float CalculateMovement (Vector3 pixelPos)
+    {
+        //Converts to a 0 to 1 scale
+        var worldPos = Camera.main.ScreenToViewportPoint(pixelPos);
+
+        float xMove = 0;
+        //If we press the right side of the screen
+        if (worldPos.x < 0.5f)
+        {
+            xMove = -1;
+        }
+        else
+        {
+            //Otherwise we're on the left
+            xMove = 1;
+        }
+
+        // replace horizontalSpeed with our own value
+        return xMove * dodgeSpeed;
+    }
+
+    /// <summary>
     /// Update is called once per frame
     /// </summary>
     void Update()
@@ -40,6 +66,27 @@ public class PlayerBehaviour : MonoBehaviour
         //check if we're moving to the side
         var horizontalSpeed = Input.GetAxis("Horizontal") * dodgeSpeed;
 
+        //If the Mouse is held down (or the screen is tapped on mobile)
+        if (Input.GetMouseButton(0))
+        {
+            //Converts to a 0 to 1 scale
+            var worldPose = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            float xMove = 0;
+
+            //If we press the right side of the screen
+            if (worldPose.x < 0.5f)
+            {
+                xMove = -1;
+            }
+            else
+            {
+                //Otherwise we're on the left
+                xMove = 1;
+            }
+
+            //replace horizotalSpeed with our own vaule
+            horizontalSpeed = xMove * dodgeSpeed;
+        }
         // Apply our auto-moving and movement forces
         rb.AddForce(horizontalSpeed, 0, rollSpeed);
     }
